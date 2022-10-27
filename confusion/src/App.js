54cc8710538491a543a5e170a044d196160cc0cd
vipul kumar <vipulsingh1262001@gmail.com>
2022-10-27 19:49:39 +0530

    m

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,26 +1,16 @@
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap ';
 import './App.css';
 
 function App() {
   return (
     
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 classname="App-title">Welcome to react</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar dark color="primary">
+        <div className="container">
+         <NavbarBrand href="/">Ristorent Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
     </div>
   );
 }
