6df3760597300805e3d94937d3669b396c7e8614
vipul kumar <vipulsingh1262001@gmail.com>
2022-10-27 19:08:41 +0530

    v

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 classname="App-title">Welcome to react</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          
+
         </p>
         <a
           className="App-link"
